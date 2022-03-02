843811b88b89cc4e444717aa404e0e7d14ccb7cd
Sean <seanms418@gmail.com>
2022-03-02 17:42:20 +1100

    added the comment model and completed index for models adding relationships

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const Comment = require("./Comment");
+const Post = require("./Post");
+const User = require("./User");
+
+User.hasMany(Post, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id"
+})
+
+Post.belongsTo(User, {
+    foreignKey: "user_id"
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id"
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Post, Comment }
+
