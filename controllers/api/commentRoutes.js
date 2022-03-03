b57892d97bfcd6853744a83837e19a80a09fbcd0
Sean <seanms418@gmail.com>
2022-03-03 16:16:22 +1100

    set up the post and comment routes, all just sending a status to see if they work

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Comment } = require('../models');
+
+router.post("/", async (res, req) => {
+    try {
+        res.statusCode(200);
+    } catch (err) {
+        res.statusCode(500).json(err);
+    }
+});
+
+router.delete("/:id", async (res, req) => {
+    try {
+        res.statusCode(200);
+    } catch (err) {
+        res.statusCode(500).json(err);
+    }
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const dashboard = require('../dashboardRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
-router.use('/dashboard', dashboard);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
+
 
 module.exports = router;
