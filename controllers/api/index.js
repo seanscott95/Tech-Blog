4dd80596d257f3f0cb2e758994821871b7790d56
Sean <seanms418@gmail.com>
2022-03-03 16:03:49 +1100

    had to move dashboardRoutes into its own folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const dashboard = require('./dashboardRoutes');
+const dashboard = require('../dashboardRoutes');
 
 router.use('/users', userRoutes);
 router.use('/dashboard', dashboard);
