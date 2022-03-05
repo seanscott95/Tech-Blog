4192427eb64d49e22c7bdeba40f6c6b986c13aca
Sean <seanms418@gmail.com>
2022-03-05 18:35:59 +1100

    added helper function variable to server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-require('dotenv').config();
+const helpers = require('./utils/helpers');
+// require('dotenv').config();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,10 +12,11 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.DB_SECRET,
+  // secret: process.end.DB_SECRET, Needed?
+  secret: "Super secret secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -25,7 +27,7 @@ const sess = {
 
 app.use(session(sess));
 
-// Informs Express.js on which template engine to use
+// Informs Express.js to use the handlebars template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -36,5 +38,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port localhost:${PORT}`));
 });
