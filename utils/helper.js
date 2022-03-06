d5c032ef4354723e941cda29418650a7779564b3
Sean <seanms418@gmail.com>
2022-03-06 18:07:05 +1100

    format_date and created_at arent working, commeneted them out for the time being

--- utils/helper.js
+++ utils/helper.js
@@ -2,9 +2,5 @@ module.exports = {
     format_date: (date) => {
         // Format date as MM/DD/YYYY
         return date.toLocaleDateString();
-    },
-    created_at: (date) => {
-        // Format time as HH/MM/SS
-        return date.toLocaleTimeString();
     }
 };
