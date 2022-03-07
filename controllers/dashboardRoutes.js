d092f98f48755385951706ae07dbdbf1f2c992fb
Sean <seanms418@gmail.com>
2022-03-07 17:26:16 +1100

    have the dashboard sho only users post

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -4,26 +4,17 @@ const withAuth = require('../utils/auth');
 
 router.get("/", withAuth, async (req, res) => {
     try {
-        const postData = await Post.findAll(req.session.user_id, {
-            include: [
-                {
-                    model: Comment,
-                    attributes: ['id', 'description', 'user_id', 'post_id', 'created_at'],
-                    include: {
-                        model: User,
-                        attributes: ['name'],
-                    }
-                },
-                {
-                    model: User,
-                    attributes: ['name']
-                },
-            ],
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
         });
 
-        const posts = postData.map((post) => post.get({ plain: true }));
+        const user = userData.get({ plain: true });
 
-        res.render('dashboard', { posts });
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+    });
     } catch (err) {
         res.status(500).json(err);
     }
