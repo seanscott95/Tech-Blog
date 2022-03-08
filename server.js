ae53d16b3ef0b71dcdec240946d620d9170ef3ae
Sean <seanms418@gmail.com>
2022-03-08 16:17:51 +1100

    added environmental variables for sessions secret variable

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helper');
-// require('dotenv').config();
+require('dotenv').config();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -15,8 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  // secret: process.end.DB_SECRET, Needed?
-  secret: "Super secret secret",
+  secret: process.end.DB_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
