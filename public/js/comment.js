b543524b2a24eef30aa9672f3d62dd2444d38875
Sean <seanms418@gmail.com>
2022-03-09 22:17:16 +1100

    moved the delete comment button from the single-post to edit-comment

--- public/js/comment.js
+++ public/js/comment.js
@@ -28,27 +28,6 @@ const commentFormHandler = async (event) => {
     }
 };
 
-const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-        const id = event.target.getAttribute('data-id');
-
-        const response = await fetch(`/api/comments/${id}`, {
-            method: 'DELETE',
-        });
-
-        if (response.ok) {
-            document.location.reload();
-        } else {
-            alert('Failed to delete comment');
-        }
-    }
-};
-
-
-document
-    .querySelector('#delete-comment')
-    .addEventListener('click', delButtonHandler);
-
 document
     .querySelector('.new-comment-form')
     .addEventListener('submit', commentFormHandler);
---
+++ public/js/edit-comment.js
@@ -0,0 +1,50 @@
+const editCommentHandler = async (event) => {
+    event.preventDefault();
+
+    // 
+    const description = document.querySelector('#description-input').value.trim();
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if (description && id) {
+        // Send a PUT request to the API endpoint
+        const response = await fetch(`/api/comments/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ description }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // If successful, redirect the browser to the dashboard
+            document.location.replace(`/dashboard`);
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/comments/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert('Failed to delete comment');
+        }
+    }
+};
+
+
+document
+    .querySelector('.edit-form')
+    .addEventListener('submit', editCommentHandler);
+
+document
+    .querySelector('#delete-comment')
+    .addEventListener('click', delButtonHandler);
